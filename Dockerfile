d445c4d07f3c5aa149f4cb95d1f2cb4ddcc0b5bb
中男顽童 <qgtxqj@gmail.com>
2019-07-08 16:22:45 +0800

    add caddy

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,19 @@ FROM alpine:latest
 ENV VER=4.19.1 UUID=4890bd47-5180-4b1c-9a5d-3ef686543112
 
 RUN apk add --no-cache --virtual .build-deps busybox bash ca-certificates wget \
- && mkdir -m 755 /etc/v2ray \
- && mkdir -m 755 /etc/caddy \
- && mkdir -m 755 /etc/caddy/www \
- && mkdir -m 755 /usr/bin/v2ray \
+ && mkdir -m 777 /etc/v2ray \
+ && mkdir -m 777 /etc/caddy \
+ && mkdir -m 777 /etc/caddy/www \
+ && mkdir -m 777 /usr/bin/v2ray \
  && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \ 
  && echo "Asia/Shanghai" > /etc/timezone
  
 ADD files/config.json /etc/v2ray/config.json
+chmod 777 /etc/v2ray/config.json
 ADD files/Caddyfile /etc/caddy/Caddyfile
+chmod 777 /etc/caddy/Caddyfile
 ADD files/index.html /etc/caddy/www/index.html
+chmod 777  /etc/caddy/www/index.html
 ADD run.sh /run.sh
 RUN chmod +x /run.sh
 ENTRYPOINT /run.sh
--- run.sh
+++ run.sh
@@ -1,15 +1,20 @@
 sed -i "s/uuid/$UUID/g" /etc/v2ray/config.json
 
-wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh
-/usr/local/caddy/caddy -conf /etc/caddy/Caddyfile >/dev/null  2>&1  &
+cd /usr/bin/v2ray
+wget --no-check-certificate -qO 'caddy.tar.gz' https://github.com/caddyserver/caddy/releases/download/v1.0.1/caddy_v1.0.1_linux_amd64.tar.gz
+tar xvf caddy.tar.gz
+rm -rf caddy.tar.gz
+chmod 777 caddy
+
+/usr/bin/v2ray/caddy -conf /etc/caddy/Caddyfile >/dev/null  2>&1  &
 
 echo "Start caddy Success!"
 
-cd /tmp
+cd /usr/bin/v2ray
 wget --no-check-certificate https://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
 unzip v2ray-linux-64.zip v2ray v2ctl geosite.dat geoip.dat -d /usr/bin/v2ray/
 rm -rf /tmp/v2ray-linux-64.zip
-chmod +x /usr/bin/v2ray/v2ray /usr/bin/v2ray/v2ctl
+chmod 777 /usr/bin/v2ray/*
 
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
 echo "Start v2ray Success!"
